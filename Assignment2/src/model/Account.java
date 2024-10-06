f6ee9c7fc33edd5266f991c686f9d44f00ed48e0
LinNEUdash <lin.yuxia@northeastern.edu>
2024-10-06 15:16:39 -0400

    Assignment2 left ui

---
+++ Assignment2/src/model/Account.java
@@ -0,0 +1,158 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.Account to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author linyuxiao
+ */
+public class Account {
+    
+    private String firstName;
+    private String lastName;
+    private int socialSecurityNumber;
+    private int age;
+    private String streetAddressHome;
+    private String unitNumberHome;
+    private String cityHome;
+    private String stateHome;
+    private int zipcodeHome;
+    private long phoneNumberHome;
+    private String streetAddressWork;
+    private String unitNumberWork;
+    private String cityWork;
+    private String stateWork;
+    private int zipcodeWork;
+    private long phoneNumberWork;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSocialSecurityNumber() {
+        return socialSecurityNumber;
+    }
+
+    public void setSocialSecurityNumber(int socialSecurityNumber) {
+        this.socialSecurityNumber = socialSecurityNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStreetAddressHome() {
+        return streetAddressHome;
+    }
+
+    public void setStreetAddressHome(String streetAddressHome) {
+        this.streetAddressHome = streetAddressHome;
+    }
+
+    public String getUnitNumberHome() {
+        return unitNumberHome;
+    }
+
+    public void setUnitNumberHome(String unitNumberHome) {
+        this.unitNumberHome = unitNumberHome;
+    }
+
+    public String getCityHome() {
+        return cityHome;
+    }
+
+    public void setCityHome(String cityHome) {
+        this.cityHome = cityHome;
+    }
+
+    public String getStateHome() {
+        return stateHome;
+    }
+
+    public void setStateHome(String stateHome) {
+        this.stateHome = stateHome;
+    }
+
+    public int getZipcodeHome() {
+        return zipcodeHome;
+    }
+
+    public void setZipcodeHome(int zipcodeHome) {
+        this.zipcodeHome = zipcodeHome;
+    }
+
+    public long getPhoneNumberHome() {
+        return phoneNumberHome;
+    }
+
+    public void setPhoneNumberHome(long phoneNumberHome) {
+        this.phoneNumberHome = phoneNumberHome;
+    }
+
+    public String getStreetAddressWork() {
+        return streetAddressWork;
+    }
+
+    public void setStreetAddressWork(String streetAddressWork) {
+        this.streetAddressWork = streetAddressWork;
+    }
+
+    public String getUnitNumberWork() {
+        return unitNumberWork;
+    }
+
+    public void setUnitNumberWork(String unitNumberWork) {
+        this.unitNumberWork = unitNumberWork;
+    }
+
+    public String getCityWork() {
+        return cityWork;
+    }
+
+    public void setCityWork(String cityWork) {
+        this.cityWork = cityWork;
+    }
+
+    public String getStateWork() {
+        return stateWork;
+    }
+
+    public void setStateWork(String stateWork) {
+        this.stateWork = stateWork;
+    }
+
+    public int getZipcodeWork() {
+        return zipcodeWork;
+    }
+
+    public void setZipcodeWork(int zipcodeWork) {
+        this.zipcodeWork = zipcodeWork;
+    }
+
+    public long getPhoneNumberWork() {
+        return phoneNumberWork;
+    }
+
+    public void setPhoneNumberWork(long phoneNumberWork) {
+        this.phoneNumberWork = phoneNumberWork;
+    }
+   
+}
